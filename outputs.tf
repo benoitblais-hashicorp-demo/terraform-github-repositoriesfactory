e30fe47936701dfcedfaffbaa73692987e332aaf
Benoit Blais <benoit.blais@hashicorp.com>
2025-11-11 11:26:00 -0500

    variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,11 @@
-output "repository" {
-  description = "Repositories within your GitHub organization."
-  value       = github_repository.this
-}
+# output "branch_protection" {
+#   description = "GitHub branch protection within your GitHub repository."
+#   value       = { for branch_protection in github_branch_protection.this : branch_protection.pattern => branch_protection }
+# }
 
 output "full_name" {
   description = "A string of the form \"orgname/reponame\"."
   value       = github_repository.this.full_name
-}
-
-output "html_url" {
-  description = "URL to the repository on the web."
-  value       = github_repository.this.html_url
-}
-
-output "ssh_clone_url" {
-  description = "URL that can be provided to git clone to clone the repository via SSH."
-  value       = github_repository.this.ssh_clone_url
-}
-
-output "http_clone_url" {
-  description = "URL that can be provided to git clone to clone the repository via HTTPS."
-  value       = github_repository.this.http_clone_url
 }
 
 output "git_clone_url" {
@@ -28,41 +13,51 @@ output "git_clone_url" {
   value       = github_repository.this.git_clone_url
 }
 
-output "svn_url" {
-  description = "URL that can be provided to svn checkout to check out the repository via GitHub's Subversion protocol emulation."
-  value       = github_repository.this.svn_url
+output "html_url" {
+  description = "URL to the repository on the web."
+  value       = github_repository.this.html_url
 }
 
-output "node_id" {
-  description = "GraphQL global node id for use with v4 API."
-  value       = github_repository.this.node_id
+output "http_clone_url" {
+  description = "URL that can be provided to git clone to clone the repository via HTTPS."
+  value       = github_repository.this.http_clone_url
 }
+
+# output "node_id" {
+#   description = "GraphQL global node id for use with v4 API."
+#   value       = github_repository.this.node_id
+# }
+
+# output "pages" {
+#   description = <<EOT
+#   The block consisting of the repository's GitHub Pages configuration with the following additional attributes:
+#     custom_404 : Whether the rendered GitHub Pages site has a custom 404 page.
+#     html_url   : The absolute URL (including scheme) of the rendered GitHub Pages site e.g. https://username.github.io.
+#     status     : The GitHub Pages site's build status e.g. building or built.
+# EOT
+#   value = var.pages != null ? {
+#     custom_404 = github_repository.this.pages.0.custom_404
+#     html_url   = github_repository.this.pages.0.html_url
+#     status     = github_repository.this.pages.0.status
+#   } : null
+# }
+
+# output "repository" {
+#   description = "Repositories within your GitHub organization."
+#   value       = github_repository.this
+# }
 
 output "repo_id" {
   description = "GitHub ID for the repository."
   value       = github_repository.this.id
 }
 
-output "primary_language" {
-  description = "The primary language used in the repository."
-  value       = github_repository.this.primary_language
+output "ssh_clone_url" {
+  description = "URL that can be provided to git clone to clone the repository via SSH."
+  value       = github_repository.this.ssh_clone_url
 }
 
-output "pages" {
-  description = <<EOT
-  The block consisting of the repository's GitHub Pages configuration with the following additional attributes:
-    custom_404 : Whether the rendered GitHub Pages site has a custom 404 page.
-    html_url   : The absolute URL (including scheme) of the rendered GitHub Pages site e.g. https://username.github.io.
-    status     : The GitHub Pages site's build status e.g. building or built.
-EOT
-  value = var.pages != null ? {
-    custom_404 = github_repository.this.pages.0.custom_404
-    html_url   = github_repository.this.pages.0.html_url
-    status     = github_repository.this.pages.0.status
-  } : null
-}
-
-output "branch_protection" {
-  description = "GitHub branch protection within your GitHub repository."
-  value       = { for branch_protection in github_branch_protection.this : branch_protection.pattern => branch_protection }
+output "svn_url" {
+  description = "URL that can be provided to svn checkout to check out the repository via GitHub's Subversion protocol emulation."
+  value       = github_repository.this.svn_url
 }
